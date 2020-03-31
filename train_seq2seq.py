47f79cf764c61a0b951edcae12cf538896bccdfd
ChaosCodes <kids937@foxmail.com>
2020-03-31 23:57:47 +0800

    init

--- train_seq2seq.py
+++ train_seq2seq.py
@@ -124,17 +124,18 @@ def get_batch(docs_source, w2i_source, docs_target, w2i_target, batch_size, batc
 	source_batch = []
 	target_batch = []
 
-
-	source_lens = [len(docs_source[p]) for p in ps]
-	target_lens = [len(docs_target[p])+1 for p in ps]
+	docs_source_len = len(docs_source)
+	docs_target_len = len(docs_target)
+	source_lens = [len(docs_source[p % docs_source_len]) for p in ps]
+	target_lens = [len(docs_target[p % docs_target_len])+1 for p in ps]
 
 	max_source_len = max(source_lens)
 	max_target_len = max(target_lens)
 
 
 	for p in ps:
-		source_seq = [w2i_source[w] for w in docs_source[p]] + [w2i_source["_PAD"]]*(max_source_len-len(docs_source[p]))			
-		target_seq = [w2i_target[w] for w in docs_target[p]] + [w2i_target["_EOS"]] + [w2i_target["_PAD"]]*(max_target_len-1-len(docs_target[p]))
+		source_seq = [w2i_source[w] for w in docs_source[p % docs_source_len]] + [w2i_source["_PAD"]]*(max_source_len-len(docs_source[p % docs_source_len]))			
+		target_seq = [w2i_target[w] for w in docs_target[p % docs_target_len]] + [w2i_target["_EOS"]] + [w2i_target["_PAD"]]*(max_target_len-1-len(docs_target[p % docs_target_len]))
 
 		source_batch.append(source_seq)
 		target_batch.append(target_seq)
