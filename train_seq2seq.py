474ebdbb713889119e9772266e7b83cc0dbcb5c2
ChaosCodes <kids937@foxmail.com>
2020-04-02 21:04:34 +0800

    fix bug

--- train_seq2seq.py
+++ train_seq2seq.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
 					model.seq_targets_length: target_lens
 				}
 				
-				loss, _ = sess.run([model.total_loss, model.train_op], feed_dict)
+				loss, _ = sess.run([model.loss, model.train_op], feed_dict)
 				total_loss += loss
 				if batch % 10 == 0:
 					tight_losses.append(loss)
