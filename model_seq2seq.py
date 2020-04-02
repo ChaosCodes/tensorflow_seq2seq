f410cb1501d34b1f5000007da4e3dc7c4e0d78bb
ChaosCodes <kids937@foxmail.com>
2020-04-02 23:47:46 +0800

    add back beamsearch

--- model_seq2seq.py
+++ model_seq2seq.py
@@ -33,16 +33,26 @@ class Seq2seq(object):
 	def get_GRU_cell(self, hidden_dim, dropout):
 		return tf.contrib.rnn.DropoutWrapper(tf.nn.rnn_cell.GRUCell(hidden_dim), input_keep_prob=dropout)
 	
-	def make_attention_decoder(self, decoder_cell, encoder_outputs, encoder_state):
-		attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(num_units=self.config.hidden_dim, memory=encoder_outputs, memory_sequence_length=self.seq_inputs_length)
-		# attention_mechanism = tf.contrib.seq2seq.LuongAttention(num_units=config.hidden_dim, memory=encoder_outputs, memory_sequence_length=self.seq_inputs_length)
-		decoder_cell = tf.contrib.seq2seq.AttentionWrapper(decoder_cell, attention_mechanism)
-		decoder_initial_state = decoder_cell.zero_state(batch_size=self.config.batch_size, dtype=tf.float32)
-		decoder_initial_state = decoder_initial_state.clone(cell_state=encoder_state)
+	def make_attention_decoder(self, decoder_cell, encoder_outputs, encoder_state, useBeamSearch):
+		if useBeamSearch > 1:
+			tiled_encoder_outputs = tf.contrib.seq2seq.tile_batch(encoder_outputs, multiplier=useBeamSearch)
+			tiled_sequence_length = tf.contrib.seq2seq.tile_batch(self.seq_inputs_length, multiplier=useBeamSearch)
+			attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(num_units=self.config.hidden_dim, memory=tiled_encoder_outputs, memory_sequence_length=tiled_sequence_length)
+			decoder_cell = tf.contrib.seq2seq.AttentionWrapper(decoder_cell, attention_mechanism)
+			tiled_encoder_final_state = tf.contrib.seq2seq.tile_batch(encoder_state, multiplier=useBeamSearch)
+			tiled_decoder_initial_state = decoder_cell.zero_state(batch_size=self.config.batch_size*useBeamSearch, dtype=tf.float32)
+			tiled_decoder_initial_state = tiled_decoder_initial_state.clone(cell_state=tiled_encoder_final_state)
+			decoder_initial_state = tiled_decoder_initial_state
+		else:
+			attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(num_units=self.config.hidden_dim, memory=encoder_outputs, memory_sequence_length=self.seq_inputs_length)
+			# attention_mechanism = tf.contrib.seq2seq.LuongAttention(num_units=config.hidden_dim, memory=encoder_outputs, memory_sequence_length=self.seq_inputs_length)
+			decoder_cell = tf.contrib.seq2seq.AttentionWrapper(decoder_cell, attention_mechanism)
+			decoder_initial_state = decoder_cell.zero_state(batch_size=self.config.batch_size, dtype=tf.float32)
+			decoder_initial_state = decoder_initial_state.clone(cell_state=encoder_state)
 		return decoder_cell, decoder_initial_state
 		
 	
-	def __init__(self, config, w2i_target, useAttention=True):
+	def __init__(self, config, w2i_target, useAttention=True, useBeamSearch=1):
 		initializer = np.random.random_sample((config.source_vocab_size, config.embedding_dim)) - 0.5
 		self.word_init = initializer.astype(np.float32)
 
@@ -56,7 +66,7 @@ class Seq2seq(object):
 			encoder_outputs, decoder_initial_state = self.encoder_layer(encoder_inputs_embedded, config.hidden_dim, config.dropout, use_bi=True)
 		
 		with tf.variable_scope("decoder", reuse=tf.AUTO_REUSE):
-			
+
 			decoder_embedding = tf.get_variable('decoder_embedding', initializer=self.word_init, dtype=tf.float32, regularizer=l2_reg)
 			tokens_go = tf.fill([config.batch_size], w2i_target["_GO"])
 
@@ -68,8 +78,16 @@ class Seq2seq(object):
 			with tf.variable_scope("gru_cell"):
 				decoder_cell = self.get_GRU_cell(self.config.hidden_dim, self.config.dropout)
 				if useAttention:
-						decoder_cell, decoder_initial_state = self.make_attention_decoder(decoder_cell, encoder_outputs, decoder_initial_state)
-			
+					decoder_cell, decoder_initial_state = self.make_attention_decoder(decoder_cell, encoder_outputs, decoder_initial_state, useBeamSearch)
+				elif useBeamSearch > 1:
+					decoder_initial_state = tf.contrib.seq2seq.tile_batch(encoder_state, multiplier=useBeamSearch)
+
+		if useBeamSearch > 1:
+			train_decoder = tf.contrib.seq2seq.BeamSearchDecoder(decoder_cell, decoder_embedding, tokens_go, w2i_target["_EOS"],  decoder_initial_state, beam_width=useBeamSearch, output_layer=tf.layers.Dense(config.target_vocab_size))
+			train_decoder_outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(train_decoder, maximum_iterations=tf.reduce_max(self.seq_targets_length))
+			predict_decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell, predict_helper, decoder_initial_state, output_layer=tf.layers.Dense(config.target_vocab_size))
+			predict_decoder_outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(predict_decoder, maximum_iterations=tf.reduce_max(self.seq_targets_length))
+		else:
 			# run decoder and get the output
 			train_decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell, train_helper, decoder_initial_state, output_layer=tf.layers.Dense(config.target_vocab_size))
 			train_decoder_outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(train_decoder, maximum_iterations=tf.reduce_max(self.seq_targets_length))
@@ -77,17 +95,21 @@ class Seq2seq(object):
 			predict_decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell, predict_helper, decoder_initial_state, output_layer=tf.layers.Dense(config.target_vocab_size))
 			predict_decoder_outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(predict_decoder, maximum_iterations=tf.reduce_max(self.seq_targets_length))
 
-		# process the out and get the likely word id
-		decoder_logits = train_decoder_outputs.rnn_output
-		self.out = tf.argmax(decoder_logits, 2)
+		if useBeamSearch > 1:
+			self.out = train_decoder_outputs.predicted_ids[:,:,0]
+			self.predict_out = predict_decoder_outputs.predicted_ids[:,:,0]
+		else:	
+			# process the out and get the likely word id
+			decoder_logits = train_decoder_outputs.rnn_output
+			self.out = tf.argmax(decoder_logits, 2)
 
-		predict_decoder_logits = predict_decoder_outputs.rnn_output
-		self.predict_out = tf.argmax(predict_decoder_logits, 2)
-		
-		sequence_mask = tf.sequence_mask(self.seq_targets_length, dtype=tf.float32)
+			predict_decoder_logits = predict_decoder_outputs.rnn_output
+			self.predict_out = tf.argmax(predict_decoder_logits, 2)
+			
+			sequence_mask = tf.sequence_mask(self.seq_targets_length, dtype=tf.float32)
 
-		self.loss = tf.contrib.seq2seq.sequence_loss(logits=decoder_logits, targets=self.seq_targets, weights=sequence_mask)
-		# self.predict_loss = tf.contrib.seq2seq.sequence_loss(logits=predict_decoder_logits, targets=self.seq_targets, weights=sequence_mask)
-		# self.total_loss = self.loss + self.predict_loss
+			self.loss = tf.contrib.seq2seq.sequence_loss(logits=decoder_logits, targets=self.seq_targets, weights=sequence_mask)
+			# self.predict_loss = tf.contrib.seq2seq.sequence_loss(logits=predict_decoder_logits, targets=self.seq_targets, weights=sequence_mask)
+			# self.total_loss = self.loss + self.predict_loss
 
-		self.train_op = tf.train.AdamOptimizer(learning_rate=config.learning_rate).minimize(self.loss)
+			self.train_op = tf.train.AdamOptimizer(learning_rate=config.learning_rate).minimize(self.loss)
