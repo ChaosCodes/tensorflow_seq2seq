694279d8186309d16e5bbf6d06761a3ae9fa9e37
ChaosCodes <kids937@foxmail.com>
2020-04-03 10:51:42 +0800

    fix

--- infer_seq2seq.py
+++ infer_seq2seq.py
@@ -54,9 +54,9 @@ if __name__ == "__main__":
 		print("samples:\n")
 		predict_batch = sess.run(model.out, feed_dict)
 		for i in range(len(source_batch)):
-			print("in:", [i2w_source[num] for num in source_batch[i] if i2w_source[num] != "_PAD"])
-			print("out:",[i2w_target[num] for num in predict_batch[i] if i2w_target[num] != "_PAD"])
-			print("tar:",[i2w_target[num] for num in target_batch[i] if i2w_target[num] != "_PAD"])
+			print("in:", ' '.join([i2w_source[num] for num in source_batch[i] if i2w_source[num] != "_PAD"]))
+			print("out:", ' '.join([i2w_target[num] for num in predict_batch[i] if i2w_target[num] != "_PAD"]))
+			print("tar:", ' '.join([i2w_target[num] for num in target_batch[i] if i2w_target[num] != "_PAD"]))
 			print("")
 			
 			
